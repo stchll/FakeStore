8255ac834217f547638d3b4e03d3273c948084f7
stepan_chepil <chepillstepan11@gmail.com>
2025-11-30 09:25:56 +0200

    global changes

--- main.js
+++ main.js
@@ -1,5 +1,37 @@
-fetch('https://fakestoreapi.com/products')
-.then(requie=>requie.json())
-.then(data=> {
-    console.log(data);
-})
+let currentPage = 1
+
+const pages = {
+    product: document.getElementById('productPage'),
+}
+
+const objects = {
+    productCard: document.getElementById('productCard'),
+}
+
+let products = []
+
+function loadCurrentProducts() {
+    let link = `https://fakestoreapi.com/products`
+
+    fetch(link)
+    .then(requie => requie.json())
+    .then(data=> {
+        for (let last of products) {
+            last.remove()
+        }
+
+        for (let product of data) {
+            let card = objects.productCard.cloneNode(true)
+            card.style.display = 'flex'
+            card.querySelector('#productImg').src = product.image
+            card.querySelector('#productDescription').textContent = product.title
+            pages.product.append(card)
+
+            products.push(card)
+        }
+    })
+}
+
+loadCurrentProducts()
+loadCurrentProducts()
+loadCurrentProducts()
