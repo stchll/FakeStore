b296b27f8863ae265f50e98769d0eb6aa14d3e4b
stepan_chepil <chepillstepan11@gmail.com>
2025-12-05 21:43:52 +0200

    potuzhno update fixes bug cart bogdan ne vmiye java skript

--- main.js
+++ main.js
@@ -5,17 +5,30 @@ const cartMenu = document.querySelector(".cartMenu")
 const wrap = document.querySelector(".wrap")
 
 let switchTimeout
+let canSwitch = true
 
-cartBtn.addEventListener("click", function () {
+cartBtn.onclick = function () {
+    if (!canSwitch) {return}
+
+    canSwitch = false
     cartMenu.style.left = "0%"
     clearTimeout(switchTimeout)
     wrap.style.opacity = "0"
     switchTimeout = setTimeout(function () {
         wrap.style.display = "none"
-    }, 300)
-})
-HomeBtn.addEventListener("click", function () {
+
+        canSwitch = true
+    }, 3 * 100)
+}
+
+HomeBtn.onclick = function () {
+    if (!canSwitch) {return}
+
+    canSwitch = false
+
     cartMenu.style.left = "-100%"
     wrap.style.opacity = "1"
     wrap.style.display = "flex"
-})
+
+    canSwitch = true
+}
